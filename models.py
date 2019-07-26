58fc6a2a6668aeb0e8ba5c5cdd878b437bd70dd6
ahassan@azavea.com <adeel.hassan93@gmail.com>
2019-07-26 17:15:48 -0400

    fixes

--- models.py
+++ models.py
@@ -193,13 +193,13 @@ class ModifiedConv_alt_add(nn.Module):
 
 		self.original_conv = nn.Sequential(
 			conv,
-			deepcopy(bn),
-			nn.ReLU()
+			deepcopy(bn).cuda(),
+			nn.ReLU().cuda()
 		)
 		self.new_conv = nn.Sequential(
 			nn.Conv2d(self.new_in, self.new_out, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False).cuda(),
-			nn.BatchNorm2d(self.new_out, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
-			nn.ReLU()
+			nn.BatchNorm2d(self.new_out, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True).cuda(),
+			nn.ReLU().cuda()
 		)
 
 	def forward(self, X):
