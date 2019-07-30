1d2a934c9959e0f74acd55d6b3470d25fabf3e4c
ahassan@azavea.com <adeel.hassan93@gmail.com>
2019-07-29 22:57:25 -0400

    add training resumption support

--- io_.py
+++ io_.py
@@ -2,6 +2,9 @@ import pickle
 import boto3
 import os
 import torch
+import numpy as np
+from glob import glob
+import re
 
 class LocalIoHandler(object):
 	def __init__(self, root='.'):
@@ -15,6 +18,9 @@ class LocalIoHandler(object):
 
 	def to_local_path(self, path):
 		return f'{self.root}/{path}'
+
+	def file_exists(self, path):
+		return os.path.isfile(self.to_local_path(path))
 
 	def load_pickled_file(self, path):
 		with open(self.to_local_path(path), 'rb') as f:
@@ -40,19 +46,39 @@ class LocalIoHandler(object):
 		}
 		torch.save(state, self.to_local_path(path))
 
+	def save_checkpoint(self, model, optimizer, sched, path, info={}):
+		state = {
+			'model': model.state_dict(),
+			'optimizer': optimizer.state_dict(),
+			'sched': sched.state_dict(),
+			'info': info
+		}
+		torch.save(state, self.to_local_path(path))
+
 	def load_saved_model(self, path):
-	    state = torch.load(self.to_local_path(path))
-	    return state
+		state = torch.load(self.to_local_path(path))
+		return state
 
 	def load_model_weights(self, model, path):
-	    state = self.load_saved_model(path)
-	    model.load_state_dict(state['model'])
+		state = self.load_saved_model(path)
+		model.load_state_dict(state['model'])
 
 	def save_log(self, path, log):
 		self.save_pickled_file(path, log)
 
 	def save_log_str(self, path, log_str):
 		self.append_to_file(f'logs/{path}', log_str)
+
+	def checkpoint_exists(self):
+		chkpt_fnames = glob(f'{self.to_local_path("checkpoints")}/epoch_*')
+		return len(chkpt_fnames) > 0
+
+	def load_latest_checkpoint(self):
+		chkpt_fnames = glob(f'{self.to_local_path("checkpoints")}/epoch_*')
+		regex = re.compile(r'epoch_(\d+).*')
+		latest_chkpt = np.argmax([int(regex.match(f).group(1)) for f in chkpt_fnames])
+		chkpt = self.load_saved_model(f'checkpoints/{chkpt_fnames[latest_chkpt]}')
+		return chkpt
 
 
 class S3IoHandler(object):
@@ -63,13 +89,14 @@ class S3IoHandler(object):
 		self.local_root = local_root
 		self.local_io_handler = LocalIoHandler(local_root)
 		self.to_local_path = self.local_io_handler.to_local_path
+		self.file_exists = self.local_io_handler.file_exists
 
 	def to_s3_path(self, path):
 		return f'{self.s3_root}/{path}'
 
 	def load_pickled_file(self, s3_path, tgt_path, force_download=False):
-		local_path = self.to_local_path(tgt_path)
-		if force_download or (not os.path.isfile(local_path)):
+		if force_download or (not self.file_exists(tgt_path)):
+			local_path = self.to_local_path(tgt_path)
 			self.s3.download_file(self.s3_bucket, s3_path, local_path)
 		return self.local_io_handler.load_pickled_file(tgt_path)
 
@@ -103,14 +130,42 @@ class S3IoHandler(object):
 		self.local_io_handler.save_model(model, path, info)
 		self.s3.upload_file(local_path, self.s3_bucket, s3_path)
 
-	def load_model_weights(self, model, s3_path, tgt_path, force_download=False):
-		local_path = self.to_local_path(tgt_path)
-		if force_download or (not os.path.isfile(local_path)):
+	def save_checkpoint(self, model, optimizer, sched, path, info={}):
+		local_path = self.to_local_path(path)
+		s3_path = self.to_s3_path(path)
+		self.local_io_handler.save_checkpoint(model, optimizer, sched, path, info)
+		self.s3.upload_file(local_path, self.s3_bucket, s3_path)
+
+	def load_saved_model(self, s3_path, path, force_download=False):
+		if force_download or (not self.file_exists(path)):
+			local_path = self.to_local_path(path)
 			self.s3.download_file(self.s3_bucket, s3_path, local_path)
-		self.local_io_handler.load_model_weights(model, tgt_path)
+		return self.local_io_handler.load_saved_model(path)
+
+	def load_model_weights(self, model, s3_path, path, force_download=False):
+		if force_download or (not self.file_exists(path)):
+			local_path = self.to_local_path(path)
+			self.s3.download_file(self.s3_bucket, s3_path, local_path)
+		self.local_io_handler.load_model_weights(model, path)
 
 	def save_log(self, path, log):
 		self.save_pickled_file(path, log)
 
 	def save_log_str(self, path, log_str):
 		self.append_to_file(path, log_str)
+
+	def ls(self, path):
+		res = self.s3.list_objects(Bucket=self.s3_bucket, Prefix=path)
+		fnames = [c['Key'] for c in res['Contents']]
+		return fnames
+
+	def checkpoint_exists(self):
+		chkpt_fnames = self.ls(self.to_s3_path(f'checkpoints/epoch_'))
+		return len(chkpt_fnames) > 0
+
+	def load_latest_checkpoint(self):
+		chkpt_fnames = self.ls(self.to_s3_path(f'checkpoints/epoch_'))
+		regex = re.compile(r'.*epoch_(\d+).*')
+		latest_chkpt = np.argmax([int(regex.match(f).group(1)) for f in chkpt_fnames])
+		chkpt = self.load_saved_model(chkpt_fnames[latest_chkpt], 'latest_checkpoint')
+		return chkpt
