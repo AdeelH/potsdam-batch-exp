25fad07adbb4fe4404533d3571bff90c3ad52855
ahassan@azavea.com <adeel.hassan93@gmail.com>
2019-07-30 12:33:46 -0400

    factor out common code from notebooks

--- monitoring.py
+++ monitoring.py
@@ -24,3 +24,44 @@ def logs_to_str(logs):
         log_str += '-----------------------------------------------------------------------------------------------\n'
 
         return log_str
+
+def track_best_model(io_handler, model, logs):
+
+    last_epoch = logs['epoch'][-1]
+
+    val_acc  = logs['val_acc' ][-1]
+    val_loss = logs['val_loss'][-1]
+
+    last_best_acc  = logs['best_acc' ][-1] if last_epoch > 0 else -1
+    last_best_loss = logs['best_loss'][-1] if last_epoch > 0 else 1e8
+
+    if val_acc > last_best_acc:
+        logs['best_acc'].append(val_acc)
+        io_handler.save_model(model, f'best_model/best_acc', info=logs)
+    else:
+        logs['best_acc'].append(last_best_acc)
+
+    if val_loss < last_best_loss:
+        logs['best_loss'].append(val_loss)
+        io_handler.save_model(model, f'best_model/best_loss', info=logs)
+    else:
+        logs['best_loss'].append(last_best_loss)
+
+
+def make_plots(io_handler, logs):
+    fig = plot_lr(logs)
+    io_handler.save_img(fig, f'visualizations/lr')
+    plt.close(fig)
+
+    fig = plot_losses(logs)
+    io_handler.save_img(fig, f'visualizations/loss')
+    plt.close(fig)
+
+    fig = plot_accs(logs)
+    io_handler.save_img(fig, f'visualizations/accuracy')
+    plt.close(fig)
+
+    stat_figs = plot_class_stats(logs)
+    for stat, fig in stat_figs:
+        io_handler.save_img(fig, f'visualizations/{stat}')
+        plt.close(fig)
